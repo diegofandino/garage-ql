e8788b5d14b5f9ee58c1eaa8901095a1aab69119
DiegoFandinoDev <diegofandino.u@gmail.com>
2026-08-03 16:11:13 -0500

    Main: fixed api-url again.

--- lib/api-url.ts
+++ lib/api-url.ts
@@ -1,11 +1,9 @@
 export function getGraphQLUrl() {
     const endpoint = process.env.GRAPH_QL_ENDPOINT;
 
-    const baseUrl =
-        process.env.BASE_API_URL ||
-        (process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
-            : undefined);
+    const baseUrl = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : process.env.BASE_API_URL;
 
     if (!baseUrl) {
         throw new Error('Missing BASE_API_URL or VERCEL_URL');
